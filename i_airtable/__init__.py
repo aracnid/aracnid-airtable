27aba66e7509d7a3213e0497eb9d313f60dc7b88
Jason Romano <aracnid@gmail.com>
2026-07-12 13:26:03 -0400

    Update init file, remove hard-coded version

--- i_airtable/__init__.py
+++ i_airtable/__init__.py
@@ -1,3 +1,9 @@
 from .connector import AirtableConnector
 
-__version__ = "0.3.0"
+try:
+    from importlib.metadata import version as _version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version as _version  # type: ignore
+
+__all__ = ["__version__", "AirtableConnector"]
+__version__ = _version("aracnid-core")
